f0be6d635848fef257017d3c248ee98eabf16ada
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-26 13:15:53 -0300

    :sparkles: feat: improve navigation with keyboard

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function playKey(keyClass) {
-  document.querySelector(keyClass).play();
+function playKey(keyId) {
+  const key = document.querySelector(keyId);
+  
+  if (key && key.localName === 'audio') {
+    key.play();
+  }
 }
 
 const keys = document.querySelectorAll('.tecla');
@@ -8,5 +12,14 @@ for (let i = 0; i < keys.length; i++) {
   keys[i].onclick = function () {
     playKey(`#som_${keys[i].classList[1]}`);
   };
-  
+
+  keys[i].onkeydown = function (event) {
+    if (event.code === "Enter" || event.code === "Space") {
+      keys[i].classList.add('ativa');      
+    }
+  }
+
+  keys[i].onkeyup = function () {
+    keys[i].classList.remove('ativa');      
+  }
 }
